e49c7ce95e45b33e5a61c7737f3d2bbdfd12d7d1
gigadot <gigadot@gmail.com>
2018-01-31 16:41:08 +0700

    fix broken servlet

--- login/src/main/java/io/muic/ooc/webapp/Webapp.java
+++ login/src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -2,6 +2,8 @@ package io.muic.ooc.webapp;
 
 import java.io.File;
 import javax.servlet.ServletException;
+
+import io.muic.ooc.webapp.service.SecurityService;
 import io.muic.ooc.webapp.servlet.HomeServlet;
 import io.muic.ooc.webapp.servlet.LoginServlet;
 import org.apache.catalina.Context;
@@ -17,14 +19,18 @@ public class Webapp {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8082);
 
+        SecurityService securityService = new SecurityService();
+
         Context ctx;
         try {
             ctx = tomcat.addWebapp("", docBase.getAbsolutePath());
             LoginServlet loginServlet = new LoginServlet();
+            loginServlet.setSecurityService(securityService);
             Tomcat.addServlet(ctx, "LoginServlet", loginServlet);
             ctx.addServletMapping("/login", "LoginServlet");
 
             HomeServlet homeServlet = new HomeServlet();
+            homeServlet.setSecurityManager(securityService);
             Tomcat.addServlet(ctx, "HomeServlet", homeServlet);
             ctx.addServletMapping("/index.jsp", "HomeServlet");
 
